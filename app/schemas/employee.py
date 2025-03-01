ed71f24cf55dbb3e7e2d98587bb43c00402d1eb1
devdsys <dimakyrytschuk@gmail.com>
2025-03-01 16:20:19 +0200

    add email validation

--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -1,11 +1,21 @@
-from pydantic import BaseModel, field_validator
 from datetime import datetime
+from pydantic import BaseModel, field_validator
 from validation.password_validation import PasswordValidator
+from email_validator import validate_email, EmailNotValidError
+
 
 class EmployeeBase(BaseModel):
     """Base employee model"""
     name: str
     email: str
+
+    @field_validator("email")
+    def validate_email(cls, v):
+        try:
+            validate_email(v)
+        except EmailNotValidError as e:
+            raise ValueError(str(e))
+        return v
 
 class EmployeeCreate(EmployeeBase):
     """Employee creation model"""
